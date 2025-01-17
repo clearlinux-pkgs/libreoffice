ef1fffa9c8182f006f634092cc7fbd00f811fb2e
Brett T. Warden <brett.t.warden@intel.com>
2025-01-17 09:46:14 -0800

    Allow specifying new version manually

--- update.sh
+++ update.sh
@@ -3,6 +3,7 @@ set -e -o pipefail
 
 PKG=libreoffice
 RELEASE_URL="https://www.libreoffice.org/download/download-libreoffice/"
+MANUAL_VERSION="$1" # Optional version to build
 
 errexit() {
 	local ret=1
@@ -21,17 +22,19 @@ echo "Checking latest released version at ${RELEASE_URL}"
 TARS=$(mktemp)
 trap "rm -f $TARS" EXIT
 curl --silent --show-error --fail "$RELEASE_URL" | (grep --perl-regex --only-matching "${PKG}-\d+(?:\.\d+)+(?:\.[a-z]+)+" ||:) | sort --reverse --unique > $TARS
-if ! grep -q "^${PKG}" $TARS; then
+if ! grep -q "^${PKG}" $TARS && [[ "" == "${MANUAL_VERSION}" ]]; then
 	errexit "no ${PKG} source tarballs found at ${RELEASE_URL}"
 fi
 NEW_VERSION=$(perl -pe "s|^${PKG}-(\d+(?:\.\d+)+).*|\$1|" "${TARS}" | head -1)
-NEW_VERSION_DIR=${NEW_VERSION%.*}
 CURRENT_VERSION="$(rpmspec --srpm -q --qf="%{VERSION}" $PKG.spec)"
 
 echo "Our current version:     ${CURRENT_VERSION}"
 echo "Latest released version: ${NEW_VERSION}"
 
-if [[ "$1" != "--force" ]]; then
+if [[ "" != "${MANUAL_VERSION}" ]]; then
+	echo "Manual version specified: ${MANUAL_VERSION}"
+	NEW_VERSION="${MANUAL_VERSION}"
+elif [[ "$1" != "--force" ]]; then
 	# Check whether new version is greater than current version
 	if (echo ${NEW_VERSION}; echo ${CURRENT_VERSION}) | sort --version-sort --check=silent; then
 		echo "Not rebuilding. Run the script directly with --force to override:"
@@ -39,6 +42,9 @@ if [[ "$1" != "--force" ]]; then
 		exit
 	fi
 fi
+
+# Drop the last component of the version number for the directory name
+NEW_VERSION_DIR=${NEW_VERSION%.*}
 
 # Update the version for source and archive tarballs in Makefile
 echo "Updating Makefile."
